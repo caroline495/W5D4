848bd0cfa65d6205a2a1d14fe136aebc1603e0be
Caroline Zhang <cczhang1@yahoo.com>
2024-02-08 13:16:29 -0800

    Add first association

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :bigint           not null
+#  student_id :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+    belongs_to(
+        :course,
+        class_name: 'Course',
+        foreign_key: :course_id,
+        primary_key: :id
+    )
 end
